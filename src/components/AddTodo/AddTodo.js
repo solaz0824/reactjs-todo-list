1af4d78b6582d61de255de54f0801f96c7b8d69a
Jordi <aejordi@gmail.com>
2021-05-20 10:19:07 +0200

    addTodo - Trying to move states between components

--- src/components/AddTodo/AddTodo.js
+++ src/components/AddTodo/AddTodo.js
@@ -3,9 +3,17 @@ import React from "react";
 import "./AddTodo.scss";
 
 export default function AddTodo() {
+  function submitTodo(event) {
+    event.defaultPrevented();
+
+    addNewTodo(event.target.value);
+  }
+
   return (
     <div className="addTodo">
-      <input type="text" placeholder="Add TODO" />
+      <form id="todoInput" onSubmit={submitTodo}>
+        <input type="text" placeholder="Add TODO" />
+      </form>
     </div>
   );
 }
--- src/components/layout/Main/Main.js
+++ src/components/layout/Main/Main.js
@@ -1,14 +1,34 @@
-import React from "react";
+import React, { Component } from "react";
 import AddTodo from "../../AddTodo/AddTodo";
 import TodoList from "../../TodoList/TodoList";
 
 import "./Main.scss";
 
-export default function Main() {
-  return (
-    <div className="main">
-      <AddTodo />
-      <TodoList />
-    </div>
-  );
+class Main extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      todos: [],
+    };
+  }
+
+  addNewTodo(title) {
+    const currentTodo = { title: title, completed: false };
+
+    this.setState((prevState) => ({
+      todos: [prevState.todos, currentTodo],
+    }));
+  }
+
+  render() {
+    return (
+      <div className="main">
+        <AddTodo />
+        <TodoList />
+      </div>
+    );
+  }
 }
+
+export default Main;
